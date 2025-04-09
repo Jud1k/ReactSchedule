4e3a2cc246535dd25e9475423b78253acce5979a
Jud1k <astronsahsa@mail.ru>
2025-04-09 23:02:40 +0500

    remove calendar

--- src/components/ScheduleMenu.tsx
+++ src/components/ScheduleMenu.tsx
@@ -1,4 +1,3 @@
-import "cally";
 import { useEffect,useState } from "react";
 
 interface Group{
@@ -68,28 +67,7 @@ export default function ScheduleMenu({ onGroupSelect }:ScheduleMenuProps) {
             </li>
           ))}
         </ul>
-      </div>
-      {/* <calendar-date class="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
-        <svg
-          aria-label="Previous"
-          className="fill-current size-4"
-          slot="previous"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
-        </svg>
-        <svg
-          aria-label="Next"
-          className="fill-current size-4"
-          slot="next"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
-        </svg>
-        <calendar-month></calendar-month>
-      </calendar-date> */}
+      </div>     
     </div>
   );
 }
