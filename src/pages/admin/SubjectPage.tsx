15a2d2c8af646dc09849e9812f95da09ae433519
Jud1k <astronsahsa@mail.ru>
2025-10-24 17:34:40 +0500

    add feature subject

--- src/pages/admin/SubjectPage.tsx
+++ src/pages/admin/SubjectPage.tsx
@@ -1,5 +1,23 @@
+import { CreateSubject } from '@/features/subject/components/CreateSubject';
+import { SubjectList } from '@/features/subject/components/SubjectList';
+
 const SubjectPage = () => {
-  return <div>GroupPage</div>;
+  return (
+    <>
+      <div className="flex items-start justify-between mb-6">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">
+            Редактирование предметов
+          </h2>
+          <p className="text-muted-foreground">
+            Управляйте предметами и их деталями
+          </p>
+        </div>
+        <CreateSubject />
+      </div>
+      <SubjectList />
+    </>
+  );
 };
 
 export default SubjectPage;
