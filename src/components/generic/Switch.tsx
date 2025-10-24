7918baeda72154a1d5dfc202e593ddcf8bbff1c6
Jud1k <astronsahsa@mail.ru>
2025-10-24 17:34:20 +0500

    add registration props

--- src/components/generic/Switch.tsx
+++ src/components/generic/Switch.tsx
@@ -1,12 +1,23 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
+  registration?: Partial<UseFormRegisterReturn>;
 }
 
-export default function Switch({ className, ...props }: SwitchProps) {
+export default function Switch({
+  className,
+  registration,
+  ...props
+}: SwitchProps) {
   return (
-    <input type="checkbox" className={cn('toggle', className)} {...props} />
+    <input
+      type="checkbox"
+      className={cn('toggle', className)}
+      {...registration}
+      {...props}
+    />
   );
 }
