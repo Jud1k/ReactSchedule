c9a317e514a3ae5e045b70b421a4cdf14fb4723a
Jud1k <astronsahsa@mail.ru>
2025-11-21 23:47:01 +0500

    remove Mobx auth-store

--- src/store/root-store.tsx
+++ src/store/root-store.tsx
@@ -1,9 +1,7 @@
-import authStore from './auth-store';
 import calendarStore from '@/features/lesson/store/calendar-store';
 import scheduleStore from '@/features/lesson/store/schedule-store';
 
 export default class RootStore {
-  authStore = authStore;
   calendarStore = calendarStore;
   scheduleStore = scheduleStore;
 }
