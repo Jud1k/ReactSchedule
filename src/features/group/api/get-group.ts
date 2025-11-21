adf0ffad49d50231ee738ee671695eba9ab1d9a8
Jud1k <astronsahsa@mail.ru>
2025-11-21 13:13:18 +0500

    update groupId to string

--- src/features/group/api/get-group.ts
+++ src/features/group/api/get-group.ts
@@ -2,7 +2,7 @@ import { queryOptions, useQuery } from '@tanstack/react-query';
 import GroupService from './service';
 import { QueryConfig } from '@/lib/react-query';
 
-export const getGroupQueryOptions = (groupId: number) => {
+export const getGroupQueryOptions = (groupId: string) => {
   return queryOptions({
     queryKey: ['groups', groupId],
     queryFn: () => GroupService.fetchGroup(groupId),
@@ -10,7 +10,7 @@ export const getGroupQueryOptions = (groupId: number) => {
 };
 
 type UseGroupOptions = {
-  groupId: number;
+  groupId: string;
   queryConfig?: QueryConfig<typeof getGroupQueryOptions>;
 };
 
